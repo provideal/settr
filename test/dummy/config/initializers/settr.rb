a68d6263181283103c68da2fda330b91e506e5eb
Torsten Sprenger <ts@metaminded.com>
2012-01-17 15:08:11 +0100

    remove autogenerated files of settr from dummy app

--- test/dummy/config/initializers/settr.rb
+++
@@ -1,8 +0,0 @@
-Settr.setup do |config|
-  config.resources      = true
-  config.header_tag     = 'h1'
-  config.header_class   = 'my_header_class'
-  config.table_class    = 'my_table_class'
-  config.link_class     = 'my_link_class'
-  config.form_class     = 'my_form_class'
-end
