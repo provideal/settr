f07a8c7d4ff3f30b37a505a2e1a1e0b08fb5696e
Peter Horn <peter.horn@provideal.net>
2012-01-23 18:52:15 +0100

    modulize

---
+++ lib/settr/controller_functions.rb
@@ -0,0 +1,77 @@
+# encoding: utf-8
+module Settr::ControllerFunctions
+
+  def new
+    @setting = SettrSetting.new
+  end
+
+  def create
+    @setting = SettrSetting.new(params[:settr_setting])
+    if @setting.select?
+      unless @setting.options.include?(@setting.value)
+        flash[:error] = t('settr.setting.not_created_value_not_in_options')
+        redirect_to settr_settings_path
+        return
+      end
+    end
+
+    if @setting.save
+      flash[:success] = t('settr.setting.created')
+    else
+      flash[:error] = t('settr.setting.not_created')
+    end
+
+    redirect_to settr_settings_path
+  end
+
+  def edit
+    @setting = SettrSetting.find(params[:id])
+  end
+
+  def update
+    @setting = SettrSetting.find(params[:id])
+    @new_setting = SettrSetting.new(params[:settr_setting])
+    if @new_setting.select?
+      unless @new_setting.options.include?(@setting.value)
+        flash[:error] = t('settr.setting.not_updated_value_not_in_options')
+        redirect_to settr_settings_path
+        return
+      end
+    end
+
+    if @setting.update_attributes(params[:settr_setting])
+      flash[:success] = t('settr.setting.updated')
+    else
+      flash[:error] = t('settr.setting.not_updated')
+    end
+    redirect_to settr_settings_path
+  end
+
+  def index
+    @settings = SettrSetting.order(:key)
+  end
+
+  def destroy
+    @setting = SettrSetting.find(params[:id])
+    if @setting.destroy
+      flash[:success] = t('settr.setting.deleted')
+    else
+      flash[:error] = t('settr.setting.not_deleted')
+    end
+    redirect_to settr_settings_path
+  end
+
+  def settings
+    @settings = Settr.alterable
+
+    if request.post?
+      params[:settr].each do |k, v|
+        setting = SettrSetting.find_by_key(k)
+        setting.value = (setting.typ == 'boolean' ? (v == '1').to_s : v)
+        setting.save!
+      end
+      flash[:success] = t('settr.settings.saved')
+      redirect_to settings_settr_settings_path
+    end
+  end
+end
